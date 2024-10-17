98ed5b396a9fd18c3a50240b61965faf8081d992
Isaías Gabriel <74021327+isaiasgabriel@users.noreply.github.com>
2024-10-17 18:22:53 -0300

    Styled-components

---
+++ src/components/Button.styles.ts
@@ -0,0 +1,39 @@
+import styled, { css } from 'styled-components'
+
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
+
+interface ButtonContainerProps {
+  variant: ButtonVariant
+}
+
+const buttonVariants = {
+  primary: 'purple',
+  secondary: 'orange',
+  success: 'green',
+  danger: 'red',
+}
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 100px;
+  height: 100px;
+  margin: 10px;
+  border: 0;
+  border-radius: 5px;
+  color: white;
+
+  ${(props) => {
+    return css`
+      background-color: ${buttonVariants[props.variant]};
+    `
+  }};
+`
+
+// Setup:
+// npm i styled-components
+// npm i @types/styled-components -D
+// vscode styled components extension
+
+// Usage:
+// 1. We create the button component using the styled-components library.
+// 2. Styled-components allows us to define both CSS and component logic together in a JS/TS file.
+// 3. In the same file, we can define the interfaces, types, component, and CSS, simplifying the structure.
