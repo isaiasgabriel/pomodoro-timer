831884ceb8836c423ae39ca2670a809ec3113b0b
Isaías Gabriel <74021327+isaiasgabriel@users.noreply.github.com>
2024-10-18 16:42:15 -0300

    Pages and routes - React Router DOM

---
+++ src/Router.tsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from 'react-router-dom'
+import { History } from './pages/History'
+import { Home } from './pages/Home'
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/history" element={<History />} />
+    </Routes>
+  )
+}
+
+// 1. npm i react-router-dom
+// 2. create the pages. src > pages > Home.tsx, History.tsx
+// 3. create the router file. src > Router.tsx
+// 4. Add the routes by wrapping every <Route/> inside the <Routes/> component
+// 5. Add the Router component inside the App.tsx file using the BrowserRouter from react-router-dom
+// 6. Be sure that the BrowserRouter wraps your Router component!!!
+
+// <BrowserRouter>
+//   <Router />
+// </BrowserRouter>
