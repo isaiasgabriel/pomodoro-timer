5fb75fea154c5087a725310b0555427d6bda1df6
Isaías Gabriel <74021327+isaiasgabriel@users.noreply.github.com>
2024-10-23 12:58:44 -0300

    Layout - creating the main box

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import { History } from './pages/History'
 import { Home } from './pages/Home'
-import { DefaultLayout } from './layouts/DefaultLayout'
+import { DefaultLayout } from './layouts/DefaultLayout/DefaultLayout'
 
 export function Router() {
   return (
